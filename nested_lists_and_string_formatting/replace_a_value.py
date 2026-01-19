6ec830add9f6f6afcb773fe8fb964cd06538dae8
Sudesh <sudeshkr861@gmail.com>
2026-01-19 18:42:07 +0530

    update

---
+++ nested_lists_and_string_formatting/replace_a_value.py
@@ -0,0 +1,34 @@
+# Write a Python program that reads a matrix from the user and replaces an old value with a new value.
+def replace_old_value_with_new_value(matrix, old_value, new_value):
+    updated_matrix=[]
+    for row in matrix:
+        updated_row = row
+        for i in range(len(row)):
+            if row[i]== old_value:
+                updated_row[i]=new_value
+        updated_matrix.append(updated_row)
+    return updated_matrix
+
+def convert_string_to_int(list_a):
+    new_list = []
+    for item in list_a:
+        num = int(item)
+        new_list.append(num)
+    return new_list
+
+
+m, n = input().split()
+m, n = int(m), int(n)
+num_list = []
+
+for i in range(m):
+    list_a = input().split()
+    list_a = convert_string_to_int(list_a)
+    num_list.append(list_a)
+
+values = input().split()
+old_value, new_value = convert_string_to_int(values)
+
+updated_matrix = replace_old_value_with_new_value(num_list,old_value,new_value)
+for i in updated_matrix:
+    print(i)
