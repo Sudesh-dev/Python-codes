a24f10ea3e5dedd8facb28e6724c624c57f34234
Sudesh <sudeshkr861@gmail.com>
2026-07-21 11:40:13 +0530

    binary rev

--- misc/binary_pattern.py
+++ misc/binary_pattern.py
@@ -8,15 +8,6 @@ def diagonal_star(n):
         print()
 
 
-def binary_pattern_rev(n):
-    for i in range(1,n+1):
-        print(" "*(n-i),end="")
-        for j in range(i):
-            if j%2==0:
-                print(1,end="")
-            else:
-                print(0,end="")
-        print()
 
 def print_binary(n):
     for i in range(n):
@@ -27,6 +18,17 @@ def print_binary(n):
                 print(0,end ="")
         print()
 
-print_binary(5)
+def binary_pattern_rev(n):
+    for i in range(n):
+        print(" "*(n-i-1),end="")
+        for j in range(i+1):
+            if (i+j)%2==0:
+                print(1,end="")
+            else:
+                print(0,end="")
+        print()
+
+
+# print_binary(5)
 binary_pattern_rev(5)
-diagonal_star(5)
+# diagonal_star(5)
